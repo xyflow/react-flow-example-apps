09a6fb3dd432c94aa988aa72cc172c2ba0af7cc2
moklick <info@moritzklack.com>
2022-09-14 18:19:40 +0200

    feat(apps): cra, next, remix

---
+++ README.md
@@ -0,0 +1,7 @@
+# React Flow Example Apps
+
+This repo contains starting points / references for projects that use [React Flow](https://reactflow.dev).
+
+- [Create React App Example](/reactflow-cra)
+- [Nextjs Example](/reactflow-next)
+- [Remix Example](/reactflow-remix)
---
+++ reactflow-cra/src/Flow/index.tsx
@@ -0,0 +1,71 @@
+import { useCallback } from 'react';
+import ReactFlow, {
+  Node,
+  useNodesState,
+  useEdgesState,
+  addEdge,
+  Connection,
+  Edge,
+} from 'reactflow';
+
+// this is important! You need to import the styles from the lib to make it work
+import 'reactflow/dist/style.css';
+
+import './Flow.css';
+
+const initialNodes: Node[] = [
+  {
+    id: '1',
+    type: 'input',
+    data: { label: 'Node 1' },
+    position: { x: 250, y: 5 },
+    className: 'light',
+  },
+  {
+    id: '2',
+    data: { label: 'Node 2' },
+    position: { x: 100, y: 100 },
+    className: 'light',
+  },
+  {
+    id: '3',
+    data: { label: 'Node 3' },
+    position: { x: 400, y: 100 },
+    className: 'light',
+  },
+  {
+    id: '4',
+    data: { label: 'Node 4' },
+    position: { x: 400, y: 200 },
+    className: 'light',
+  },
+];
+
+const initialEdges: Edge[] = [
+  { id: 'e1-2', source: '1', target: '2', animated: true },
+  { id: 'e1-3', source: '1', target: '3', animated: true },
+];
+
+function Flow() {
+  const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
+  const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
+  const onConnect = useCallback(
+    (params: Connection | Edge) => setEdges((eds) => addEdge(params, eds)),
+    [setEdges]
+  );
+
+  return (
+    <div className="Flow">
+      <ReactFlow
+        nodes={nodes}
+        onNodesChange={onNodesChange}
+        edges={edges}
+        onEdgesChange={onEdgesChange}
+        onConnect={onConnect}
+        fitView
+      />
+    </div>
+  );
+}
+
+export default Flow;
