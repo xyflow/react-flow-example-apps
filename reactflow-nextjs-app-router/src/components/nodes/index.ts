2a41cf02712e87ce744ac34bf95c8d8e1fe82e55
moklick <info@moritzklack.com>
2024-06-05 13:23:39 +0200

    chore(apps): use latet version

--- reactflow-nextjs-app-router/src/components/nodes/index.ts
+++ reactflow-nextjs-app-router/src/components/nodes/index.ts
@@ -1,27 +1,32 @@
-import type { BuiltInNode, Node, NodeTypes } from "@xyflow/react";
+import type { BuiltInNode, Node, NodeTypes } from '@xyflow/react';
 import PositionLoggerNode, {
   type PositionLoggerNode as PositionLoggerNodeType,
-} from "./PositionLoggerNode";
+} from './PositionLoggerNode';
 
 export const initialNodes = [
-  { id: "a", type: "input", position: { x: 0, y: 0 }, data: { label: "wire" } },
   {
-    id: "b",
-    type: "position-logger",
-    position: { x: -100, y: 100 },
-    data: { label: "drag me!" },
+    id: 'a',
+    type: 'input',
+    position: { x: 0, y: 0 },
+    data: { label: 'wire' },
   },
-  { id: "c", position: { x: 100, y: 100 }, data: { label: "your ideas" } },
   {
-    id: "d",
-    type: "output",
+    id: 'b',
+    type: 'position-logger',
+    position: { x: -100, y: 100 },
+    data: { label: 'drag me!' },
+  },
+  { id: 'c', position: { x: 100, y: 100 }, data: { label: 'your ideas' } },
+  {
+    id: 'd',
+    type: 'output',
     position: { x: 0, y: 200 },
-    data: { label: "with React Flow" },
+    data: { label: 'with React Flow' },
   },
 ] satisfies Node[];
 
 export const nodeTypes = {
-  "position-logger": PositionLoggerNode,
+  'position-logger': PositionLoggerNode,
   // Add any of your custom nodes here!
 } satisfies NodeTypes;
 
